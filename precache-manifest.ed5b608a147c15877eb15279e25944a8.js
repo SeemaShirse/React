a5d47a3a663653186c38388275c008b0bc63952d
Shailesh Jadhav <shailesh@leometric.com>
2022-02-18 11:38:56 +0530

    Updates

---
+++ precache-manifest.ed5b608a147c15877eb15279e25944a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "773bcfaf0365a677741e",
+    "url": "/React/static/css/main.83cccee9.chunk.css"
+  },
+  {
+    "revision": "773bcfaf0365a677741e",
+    "url": "/React/static/js/main.773bcfaf.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/React/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "743f2e021b8f690c5dda",
+    "url": "/React/static/js/2.743f2e02.chunk.js"
+  },
+  {
+    "revision": "d8e7e9e079be770033e2",
+    "url": "/React/static/js/runtime~main.d8e7e9e0.js"
+  },
+  {
+    "revision": "effe48a01ccf0fa55060c7868bd25a71",
+    "url": "/React/index.html"
+  }
+];
